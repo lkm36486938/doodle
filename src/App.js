da8d52a99d1d20b357502366ee9919db7cb4e265
lkm36486938 <ksan3940@gmail.com>
2021-04-08 21:17:40 +0900

    input border effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
   useEffect(() => {
-    console.log('doodle!')
-  }, [])
-  
+    console.log("doodle!");
+  }, []);
+
+  const _onFocus = e => {
+    e.target.classList.add("onFocus");
+  };
+
+  const _onBlur = e => {
+    e.target.classList.remove("onFocus");
+  };
+
   return (
     <div className="App">
       <div className="container">
-        <div className="balloon_01"></div>
-        <div className="balloon_02"></div>
-        <div className="balloon_03"></div>
+        <input type="text" className="input" />
+        <input type="text" className="input anotherColor" />
+        <input type="text" className="input" />
+        <input type="text" className="input anotherColor" />
+        <input type="text" className="input" />
+        <hr />
+
+        {/* 2번째방법. event handler 사용 */}
+        <input
+          type="text"
+          className="event"
+          onFocus={e => e.target.classList.add("onFocus")}
+          onBlur={e => e.target.classList.remove("onFocus")}
+        />
+        <input
+          type="text"
+          className="event"
+          onFocus={_onFocus}
+          onBlur={_onBlur}
+        />
+        <input type="text" className="event" />
+        <input type="text" className="event" />
+        <input type="text" className="event" />
       </div>
     </div>
   );
